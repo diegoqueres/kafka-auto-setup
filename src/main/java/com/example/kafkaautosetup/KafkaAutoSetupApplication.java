b32854432de4ed4804e84b1e6b42ebf84672360c
Diego Queres <diegoqueres81@gmail.com>
2023-03-18 15:38:59 -0300

    Initial commit

---
+++ src/main/java/com/example/kafkaautosetup/KafkaAutoSetupApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafkaautosetup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaAutoSetupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaAutoSetupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kafkaautosetup/KafkaAutoSetupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafkaautosetup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaAutoSetupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
